9c4b362fb795c6c8d40ffdf82d3c9de4f9488700
n <nvnivs@gmail.com>
2014-03-26 13:11:27 +0000

    Updating assembly info from the build script

--- build.ps1
+++ build.ps1
@@ -2,20 +2,30 @@ Properties {
 	$baseDir = Resolve-Path .
 	$project = Split-Path $baseDir -Leaf
 	$artifactDir = "$baseDir\Build\Packages"
-	$version = "1.0.0"
+	$version = "1.0.1"
 }
 
 Task Default -Depends Build
-
 Task Clean {
 	if (Test-Path "$baseDir\build") {
 		Remove-Item "$baseDir\Build" -Recurse -Force -ErrorAction Stop
 	}
 }
-task RestorePackages {
+Task RestorePackages {
 	Exec { .\.nuget\NuGet restore -PackagesDirectory .\packages }
 }
-Task Build -Depends Clean,RestorePackages {
+Task UpdateAssemblyInfo {
+	(Get-ChildItem -Path $baseDir -Filter AssemblyInfo.cs -Recurse) |
+		Foreach-Object {
+			(Get-Content $_.FullName) |
+				Foreach-Object {
+					$_ -replace 'AssemblyVersion.+$',"AssemblyVersion(`"$version`")]" `
+					-replace 'AssemblyFileVersion.+$',"AssemblyFileVersion(`"$version`")]"
+			} |
+			Out-File $_.FullName
+		}
+}
+Task Build -Depends Clean,RestorePackages,UpdateAssemblyInfo {
 	Exec { msbuild "$project.sln" /p:Configuration=Release /t:"Clean,Build" }
 }
 Task Package -Depends Build {
@@ -24,7 +34,6 @@ Task Package -Depends Build {
 	}
 	Exec { .\.nuget\NuGet pack $baseDir\$project\$project.csproj -OutputDirectory $artifactDir -Properties Configuration=Release -Version $version }
 }
-
 Task Publish -Depends Package {
 	Exec { .\.nuget\NuGet push "$artifactDir\$project.$version.nupkg" }
 }
